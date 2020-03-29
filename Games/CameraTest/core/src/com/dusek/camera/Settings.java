1810b57ffd035298053a81d30ea21ecb382444ab
DuskoDusko <58344946+DuskoDusko@users.noreply.github.com>
2020-03-29 16:25:05 +0200

    cameraTest

---
+++ Games/CameraTest/core/src/com/dusek/camera/Settings.java
@@ -0,0 +1,7 @@
+package com.dusek.camera;
+
+public class Settings {
+    static final int WORLD_WIDTH = 500;
+    static final int WORLD_HEIGHT = 500;
+    static final float rotationSpeed = 1f;
+}
---
+++ Games/CameraTest/desktop/src/com/dusek/camera/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dusek.camera.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dusek.camera.cameraTest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new cameraTest(), config);
+	}
+}
