53179131ffeaac31cc71f88cd055cd9b7218f816
DuskoDusko <58344946+DuskoDusko@users.noreply.github.com>
2020-07-09 21:32:33 +0200

    while loop

--- Learn/src/ALL/loops/ForLoop.java
+++ Learn/src/ALL/loops/ForLoop.java
@@ -5,5 +5,8 @@ public class ForLoop {
         for (int i = 0; i < 10; i++){
             System.out.println(i);
         }
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
     }
 }
---
+++ Learn/src/ALL/loops/WhileLoop.java
@@ -0,0 +1,11 @@
+package ALL.loops;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        char ch = 'a';
+        while (ch != 'z'){
+            System.out.println(ch);
+            ch++;
+        }
+    }
+}
